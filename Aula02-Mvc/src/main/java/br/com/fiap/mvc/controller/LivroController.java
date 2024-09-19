389792c31da7aa65ae2b5e98624a85be306c3e14
Thiago Yamamoto <thiagoyama@gmail.com>
2024-09-19 20:59:55 -0300

    CRUD

--- Aula02-Mvc/src/main/java/br/com/fiap/mvc/controller/LivroController.java
+++ Aula02-Mvc/src/main/java/br/com/fiap/mvc/controller/LivroController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +17,30 @@ public class LivroController {
 
     @Autowired
     private LivroRepository livroRepository;
+
+    @PostMapping("editar")
+    @Transactional
+    public String editar(Livro livro, Model model){
+        livroRepository.save(livro);
+        model.addAttribute("msg", "Livro atualizado");
+        model.addAttribute("books", livroRepository.findAll());
+        return "livro/lista";
+    }
+
+    @GetMapping("editar/{id}")
+    public String editar(@PathVariable("id") Long id, Model model){
+        //Pesquisar o livro pelo id e enviar o livro para a view
+        model.addAttribute("livro", livroRepository.findById(id));
+        //Retornar a view
+        return "livro/editar";
+    }
+
+    @GetMapping("listar") //localhost:8080/livro/listar
+    public String listar(Model model){
+        //Enviar a lista de livros para a view
+        model.addAttribute("books", livroRepository.findAll());
+        return "livro/lista";
+    }
 
     @GetMapping("cadastrar")
     public String cadastrar(Livro livro){
