52ecf48eaebfb29b46aa3652093656c8d3402fe4
Thiago Yamamoto <thiagoyama@gmail.com>
2024-10-31 20:28:11 -0300

    Autenticação

--- Aula02-Mvc/src/main/java/br/com/fiap/mvc/config/SecurityConfigurations.java
+++ Aula02-Mvc/src/main/java/br/com/fiap/mvc/config/SecurityConfigurations.java
@@ -2,9 +2,11 @@ package br.com.fiap.mvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +15,25 @@ public class SecurityConfigurations {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**", "/login", "/webjars/**", "/css/**").permitAll()
+                        .requestMatchers("/admin/**", "/usuario/registrar").hasRole("ADMIN") // Restringe o acesso para ADMIN
+                        .requestMatchers("/user/**").hasRole("USER") // Restringe o acesso para USER
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Página de login personalizada
+                        .defaultSuccessUrl("/home") // Página de redirecionamento após sucesso no login
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout") // Redirecionamento após logout
+                );
+        return http.build();
     }
 
 }
---
+++ Aula02-Mvc/src/main/java/br/com/fiap/mvc/controller/LoginController.java
@@ -0,0 +1,17 @@
+package br.com.fiap.mvc.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String login(){
+        return "login";
+    }
+
+}
